ecdb69a5e8588899abeed74ef73d90639ca6e7d3
sagarsaini010 <sagarsaini954818@gmail.com>
2025-09-22 20:38:18 +0530

    hey

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.originalUrl}`);
+
+  next();
+});
+
+app.get("/", (req, res) => {
+  fs.readdir(`./files`, function (err, files) {
+    res.render("index", { files: files });
+  });
+});
+
+  app.post("/create", (req, res) => {
+    fs.writeFile(
+      `./files/${req.body.title.split(" ").join("")}.txt`,
+      req.body.details,
+      (err) => {
+        if (err) throw err;
+        else res.redirect("/");
+      }
+    );
+});
+
+
+ app.get("/file/:filename", (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`,'utf-8',(err,filedata)=>{
+      if (err) throw err
+      else res.render('show', {filename : req.params.filename, filedata : filedata} )
+      
+    })
+  });
+
+ app.get('/edit/:filename',(req,res)=>{
+     res.render('edit',{filename: req.params.filename})
+ }) 
+
+app.post('/edit',(req,res)=>{
+  
+  fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}.txt`,(err)=>{
+    res.redirect('/')
+  })
+   
+})
+
+
+app.listen(3000, () => {
+  console.log("it's running....");
+});
